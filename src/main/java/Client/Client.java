bd9d70c749fafff861e220e3855698c61bb19dfc
duyanh.pham <duyanh.pham@inno4b.de>
2019-04-16 17:33:11 +0200

    add test for Client

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -5,6 +5,15 @@ import java.net.InetAddress;
 public class Client {
     private int id;
     private InetAddress IP;
+    private short port;
+
+    public short getPort() {
+        return this.port;
+    }
+
+    public void setPort(short port) {
+        this.port = port;
+    }
 
     public Client() {
     }
@@ -29,4 +38,13 @@ public class Client {
         byte[] address = this.IP.getAddress();
         return address;
     }
+
+    public byte[] getPortNumAsBytes(){
+        byte[] data = new byte [2];
+        data[1] = (byte) (this.port & 0xFF);
+        data[0] = (byte) ((this.port >> 8) & 0xFF);
+        return data;
+    }
+
+    //TODO: write test
 }
---
+++ src/test/java/Client/ClientTest.java
@@ -0,0 +1,49 @@
+package Client;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+public class ClientTest {
+    @BeforeClass
+    public static void createDummies() {
+
+    }
+
+    @Test
+    public void getterSetterTest() throws UnknownHostException {
+        Client client = new Client();
+        byte[] rawIP = new byte[]{(byte) 192, (byte) 168, (byte) 1, (byte) 1};
+        InetAddress IP = InetAddress.getByAddress(rawIP);
+        Random random = new Random();
+        int ranID = random.nextInt(100);
+        int port = random.nextInt(65535);
+
+        //Setter
+        client.setId(ranID);
+        client.setIP(IP);
+        client.setPort(port);
+
+        //Getter
+        Assert.assertEquals(IP, client.getIP());
+        Assert.assertEquals(ranID, client.getId());
+        Assert.assertEquals(port, client.getPort());
+    }
+
+    @Test
+    public void isShouldReturnCorrectBytesCodeFromPort() {
+        Client client = new Client();
+        client.setPort(8080);
+        byte[] port_in_bytes = new byte[]{(byte) 0x1F, (byte) 0x90};
+        byte[] clientRawPort = client.getPortNumAsBytes();
+        Assert.assertArrayEquals(port_in_bytes, clientRawPort);
+        ByteBuffer buffer = ByteBuffer.wrap(port_in_bytes);
+        short num = buffer.getShort();
+        Assert.assertEquals(num,8080);
+    }
+}
