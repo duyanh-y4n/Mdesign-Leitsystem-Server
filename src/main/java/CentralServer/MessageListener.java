bd7c168e1ad7dd71827da5aa0e17271186850050
duyanh.pham <duyanh.pham@inno4b.de>
2019-04-20 15:46:43 +0200

    Communication server now only listen to Leitsystem clients

--- src/main/java/CentralServer/MessageListener.java
+++ src/main/java/CentralServer/MessageListener.java
@@ -1,8 +1,10 @@
 package CentralServer;
 
 import com.y4n.UDP.UDPUnicast;
+import com.y4n.Utils.MessageUtils.RequestTypes;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 
 public class MessageListener extends Thread {
     private UDPUnicast listener;
@@ -14,18 +16,29 @@ public class MessageListener extends Thread {
     @Override
     public void run() {
         try {
-            while (true){
-                String message = listener.getPacketBodyAsStr(20);
-                if (!message.isEmpty()){
-                    System.out.println("Listening...:" + message);
+            while (true) {
+                DatagramPacket packet = this.listener.getPacket(20);
+                if (packetIsFromLeitsystemClient(packet)) {
+                    System.out.println("Request from Leitsystem Client at: " + packet.getAddress().getHostAddress() + ":" + packet.getPort());
                 }
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public int getPort() {
         return this.listener.getPort();
+    }
+
+    private boolean packetIsFromLeitsystemClient(DatagramPacket packet) {
+        byte[] leitsystemRequestType = {RequestTypes.CREATE, RequestTypes.UPDATE, RequestTypes.READ, RequestTypes.DELETE};
+        short requestTypePosInHeader = 0;
+        for (int i = 0; i < leitsystemRequestType.length; i++) {
+            if (packet.getData()[requestTypePosInHeader] == leitsystemRequestType[i]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
